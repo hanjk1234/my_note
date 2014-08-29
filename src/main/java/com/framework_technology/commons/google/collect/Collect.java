34293b4fb6550a4f83b48a68e5b00bb24db792cc
GourdErwa <gourderwa@163.com>
2014-08-30 01:46:31 +0800

    update some file

--- src/main/java/com/framework_technology/commons/google/collect/Collect.java
+++ src/main/java/com/framework_technology/commons/google/collect/Collect.java
@@ -22,6 +22,18 @@
  * Email :	gourderwa@163.com
  */
 
+/*
+ * Written by wei.Li and released to the public domain
+ * Welcome to correct discussion as explained at
+ *
+ * -----------------------------------------------------------------
+ *
+ * GitHub:  https://github.com/xiaohulu
+ * Blog  :	http://blog.csdn.net/xiaohulunb
+ * WeiBo :	http://www.weibo.com/xiaohulunb  	@GourdErwa
+ * Email :	gourderwa@163.com
+ */
+
 package com.framework_technology.commons.google.collect;
 
 import com.google.common.collect.*;
